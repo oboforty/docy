b74ec2d899846c7b7cac4a45b1358ab9e0ca3a99
oboforty <rajmund.csombordi@gmail.com>
2019-02-17 20:03:27 +0100

    Replaced static folder and added listing page

--- website/scan/urls.py
+++ website/scan/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 app_name = 'scan'
 urlpatterns = [
-    path('', views.index, name='scan'),
+    # path('', views.index, name='dashboard'),
+
+    path('scans/', views.scan_list, name='scans'),
+    path('patients/', views.patient_list, name='patients'),
+
     path('upload_form/', views.change, name='upload_form'),
     path('test/', views.test, name='test'),
+
+
 ]
--- website/scan/views.py
+++ website/scan/views.py
@@ -1,26 +1,53 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect, reverse
 from .models import Patient, Scan, Diagnosis
+from typing import Sequence
 
 
-def index(request):
+# def index(request: HttpRequest):
+# 	"""
+# 	Doctor's dashboard
+# 	- lists patients
+#
+# 	"""
+#
+#
+#
+# 	return render(request, 'index.html', {
+#
+# 	})
+
+def scan_list(request: HttpRequest):
 	"""
 	List of scans
 	"""
 
-	params = {}
+	scans: Sequence[Scan] = Scan.objects.all()
 
-	return render(request, 'index.html', params)
+	return render(request, 'scan/list.html', {
+		'scans': scans
+	})
+
+def patient_list(request: HttpRequest):
+	"""
+	List of patients
+	"""
 
 
-def change(request):
+	patients: Sequence[Patient] = Patient.objects.all()
+
+	return render(request, 'patient/list.html', {
+		'patients': patients
+	})
+
+def change(request: HttpRequest):
 	"""
 	TEST PAGE
 	"""
 
 	return render(request, 'upload_form.html')
 
-def test(request):
+def test(request: HttpRequest):
 	"""
 	TEST PAGE
 	"""
